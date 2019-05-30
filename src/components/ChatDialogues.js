0edea248d9bcc57355815fdd36e1c7b5ba114cef
HadiMh <hadih7899@gmail.com>
2019-05-30 12:53:21 +0430

    added chatMessageSelf for the messages that are sent by us"

--- src/components/ChatDialogues.js
+++ src/components/ChatDialogues.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import ChatMessageSelf from './ChatMessageSelf.js';
+
 import '../css/ChatDialogues.css';
 
 const ChatDialogues = props => {
   return (
     <div className="chat_dialogues">
-      hello
+      <ChatMessageSelf/>
     </div>
   )
 }
---
+++ src/components/ChatMessageSelf.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../css/ChatMessageSelf.css';
+
+const ChatMessageSelf = props => {
+  return (
+    <div className="chat_message_self">
+      <div className="chat_message_img">
+        <img src="https://css-tricks.com/wp-content/themes/CSS-Tricks-17/ads/wufoo/600x500_clocks_yellow.png"/>
+      </div>
+      <div className="chat_message-message selfMessage">
+        <p>سمشنیبتشسیمنبت شسکمیبتشس کیهخبتشکسیخهتب سهتبثکهخصت یسمنتبشکخسیمتبکشسیهمتب مسیشتبشثصخهتزکرخهیتکبخهت</p>
+      </div>
+    </div>
+  )
+}
+
+export default ChatMessageSelf;
