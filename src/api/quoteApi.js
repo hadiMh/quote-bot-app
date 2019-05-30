8bc98873c58ec3546f07b48fd91a6ffd85786b5d
HadiMh <hadih7899@gmail.com>
2019-05-30 22:47:46 +0430

    share button now works

--- src/api/quoteApi.js
+++ src/api/quoteApi.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
-export default axios.create({
+export const quoteApi = axios.create({
   baseURL: "https://gist.githubusercontent.com/balonet/ca75e757cb292f7637505ddcb8eba8df/raw/ae885395d516df26b6c5aba657273251437b6619/Quotations.json"
 });
+
