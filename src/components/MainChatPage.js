cafa6b99bf9088ce466df2e410b060c0204f064a
HadiMh <hadih7899@gmail.com>
2019-05-30 17:15:09 +0430

    show message on the chat list with map function and also made the request possible

--- src/components/MainChatPage.js
+++ src/components/MainChatPage.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import quoteApi from '../api/quoteApi.js';
 
 import ChatHeader from './ChatHeader.js';
 import ChatInput from './ChatInput.js';
@@ -7,6 +9,30 @@ import ChatDialogues from './ChatDialogues.js';
 import '../css/MainChatPage.css';
 
 const MainChatPage = props => {
+  const [jsonApiData, setJsonApiData] = useState({})
+
+  const organizeJsonData = (jsonData) => {
+    let result = {};
+    jsonData.data.Quotations.forEach(element => {
+      if(!result.hasOwnProperty(element.Author)){
+        result[element.Author] = [element.Quotation]
+      }else{
+        result[element.Author].push(element.Quotation)
+      }
+    });
+    console.log(result);
+  }
+
+  useEffect( () => {
+    async function fetchData() {
+      console.log("use effect runs")
+      const response = await quoteApi.get('/');
+      console.log(response);
+      organizeJsonData(response);
+    }
+    fetchData();
+  }, [])
+
   return(
     <div className="main-chat-page">
       <ChatHeader chatTitle="Quote Bot"/>
