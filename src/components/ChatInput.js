de9751db7bf463f9fbdae6d39cb42931747428e3
HadiMh <hadih7899@gmail.com>
2019-05-30 14:49:40 +0430

    design almost ready. Now it's better to move to the login part and then come back to the design again if needed

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -6,8 +6,11 @@ import sendImg from '../imgs/send-img.png';
 const ChatInput = props => {
   return (
     <div className="chat_input">
+      <div className="chat_input-atach-btn">
+        <img src="https://img.icons8.com/ultraviolet/40/000000/attach.png"/>
+      </div>
       <div className="chat_input_input-div">
-        <input/>
+        <input placeholder="متن خود را وارد کنید"/>
       </div>
       <div className="chat_input-send-btn">
         <img src={sendImg}/>
